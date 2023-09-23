a5ab8f667fdb5bd7ecc3cefed7ab2a3a55575546
GavPri <gavinprior5@gmail.com>
2023-09-23 14:14:15 +0000

    deployment commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+realease : python manage.py makemigrations && python manage.py migrate
+web: gunicorn hike_scout.wsgi
--- hike_scout/settings.py
+++ hike_scout/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
@@ -31,13 +32,22 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10, 
+    'DATETIME_FORMAT': '%d %b %Y',
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer'
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'hike-scout-auth'
 JWT_AUTH_REFRESH_COOKIE = 'hike-scout-refresh-cookie'
+JW_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZERS' : 'hike_scout.serializers.CurrentUserSerializers'
@@ -47,12 +57,16 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jap%n==@$#b*$2l4r6bbw+tmp%57_l^sm9ofhdkpzx^4yty&rl'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-gavpri-hikescoutapi-0cp6fkt7xx2.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = [
+    '8000-gavpri-hikescoutapi-0cp6fkt7xx2.ws-eu104.gitpod.io',
+    'localhost',
+    'hike_scout_api.herokuapp.com'
+    ]
 
 
 # Application definition
@@ -74,10 +88,12 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'corsheaders',
     'profiles',
 ]
 SITE_ID = 1
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -86,6 +102,17 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+        ]
+else:
+    CORS_ALLOWED_ORIGINS = [
+        r"^https://.*\.gitpod\io$",
+        ]
+
+CORS_ALLOW_CREDENTIALS = True       
 
 ROOT_URLCONF = 'hike_scout.urls'
 
@@ -110,13 +137,17 @@ WSGI_APPLICATION = 'hike_scout.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES ={
+        'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 # Password validation

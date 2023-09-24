90611880dbf4008dea22de07deb34fe6b6572b4a
GavPri <gavinprior5@gmail.com>
2023-09-24 11:20:45 +0000

    add get follower id to profile serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -11,6 +11,16 @@ class ProfileSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Followers.objects.filter(
+                owner=user, follower=obj.owner
+            ).first()
+            
+            return following.id if following else None
+        return None
+
     class Meta:
         model = Profile
         fields = [

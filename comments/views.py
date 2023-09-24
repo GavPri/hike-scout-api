5c5b5ebccf7114c51c44475af29d702217e953ee
GavPri <gavinprior5@gmail.com>
2023-09-24 13:17:51 +0000

    comment filtering added

--- comments/views.py
+++ comments/views.py
@@ -2,12 +2,20 @@ from rest_framework import generics, permissions
 from .models import Comments
 from .serializers import CommentsSerializer
 from hike_scout.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     queryset = Comments.objects.all()
+
+    filter_backends = [
+        DjangoFilterBackend
+    ]
+    filterset_fields = [
+        'post'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.use)
